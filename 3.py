0da46bd87cd69157a3bf049e900c4c873270aef4
yuhan8954 <yuhan8954@gmail.com>
2025-02-06 02:49:43 +0900

    미친짓 하는중

---
+++ 3.py
@@ -0,0 +1,7 @@
+import random
+while True:
+    for i in range(10000):
+        a = random.random()
+        print(a)
+    break
+print("사실 이정도면 나머지는 안해도 돼는게 아닐까요?")
---
+++ 4.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.forward(100)
+turtle.right(60)
+turtle.backward(100)
+turtle.right(60)
+turtle.forward(100)
+#이거 진짜 오랜만에 써보네요
+#이니 파이썬 자체를 오랜만에 하는구나
+#정삼각형 완성!
+turtle.goto(0,0)
+turtle.home
+for i in range(1000):
+    turtle.circle(-i)
+#음 다음으로 넘어갑시다
+#솔직히 이건 너무 노가다에요
